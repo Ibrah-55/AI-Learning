588ee7550bcf5a548f499a45412843f183a43dfb
Ibrah-55 <kariukiibrahim55@gmail.com>
2025-03-22 00:59:45 +0300

    Trigonometry

---
+++ trigonometry.py
@@ -0,0 +1,8 @@
+import numpy as np
+
+theta = np.linspace(0, np.pi, 3)
+
+print("theta      = ", theta)
+print("sin(theta) = ", np.sin(theta))
+print("cos(theta) = ", np.cos(theta))
+print("tan(theta) = ", np.tan(theta))
