acb5b169738a5185fc0d3744c1fad536dfd96838
Ibrah-55 <kariukiibrahim55@gmail.com>
2025-03-22 01:01:57 +0300

    exponents and logs

---
+++ exponentslogs.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+x = [1, 2, 4, 10]
+print("x        =", x)
+print("ln(x)    =", np.log(x))
+print("log2(x)  =", np.log2(x))
+print("log10(x) =", np.log10(x))
+
+print("x     =", x)
+print("e^x   =", np.exp(x))
+print("2^x   =", np.exp2(x))
+print("3^x   =", np.power(3, x))
