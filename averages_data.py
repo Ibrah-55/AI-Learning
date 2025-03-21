32908cbed7fdbd68b1d51092e64ef6e581d38147
Ibrah-55 <kariukiibrahim55@gmail.com>
2025-03-22 01:13:29 +0300

    plot histogram

--- averages_data.py
+++ averages_data.py
@@ -1,7 +1,13 @@
-import pandas as pd 
+import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+sns.set()
+
 data = pd.read_csv('data/president_heights.csv')
 heights = data['height(cm)']
+
 print("Heights: ", heights)
 print("Mean height:       ", heights.mean())
 print("Standard deviation:", heights.std())
@@ -10,3 +16,10 @@ print("Maximum height:    ", heights.max())
 print("25th percentile:   ", np.percentile(heights, 25))
 print("Median:            ", np.median(heights))
 print("75th percentile:   ", np.percentile(heights, 75))
+
+plt.hist(heights, bins=10, edgecolor='black', alpha=0.7)
+plt.title('Height Distribution of US Presidents')
+plt.xlabel('Height (cm)')
+plt.ylabel('Number of Presidents')
+
+plt.show()
